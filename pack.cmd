a55956bb248bfdfcf1573958fbdd3d18bf8a5c76
Denis Stepulenok <super.denis@gmail.com>
2013-07-04 03:18:48 +0400

    Save changes

--- pack.cmd
+++ pack.cmd
@@ -3,4 +3,4 @@ git clean -Xdf
 del C:\lisiynos.zip
 SET PATH=C:\Program Files (x86)\WinRAR;C:\Program Files\WinRAR;%PATH%
 cd C:\lisiynos
-winrar a -r E:\lisiynos.zip "*.*" -x*\.git\*
+winrar a -r C:\lisiynos.zip "*.*" -x*\.git\*
