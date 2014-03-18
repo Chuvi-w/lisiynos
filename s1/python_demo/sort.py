014f9738e89ed0c4074beef4fba051429d27b32c
Denis Stepulenok <super.denis@gmail.com>
2014-03-18 17:29:11 +0400

    Save changes

--- s1/python_demo/sort.py
+++ s1/python_demo/sort.py
@@ -6,29 +6,33 @@ class A:
         self.name = name
         self.age = age
 
-a = [A("Петя", 10),  A("Вася", 15.3),
+
+a = [A("Петя", 10), A("Вася", 15.3),
      A("Лена", 2.45), A("Галя", 10),
      A("Митя", 12)]
 a.sort(key=lambda x: x.age)
 for i in a:
-   print(i.name,i.age)
+    print(i.name, i.age)
 
 b = [230.123, 234.22, 23.7, 6.3]
 b.sort(reverse=True)
 sortedB = sorted(b)
 print(sortedB)
 
+
 # Простые числа
 # Простое число - делится только на себя и на 1
 def is_simple(a):
     #  a = b * c
     s = int(sqrt(a)) + 1
-    for i in range(2, s):
-        if a % i == 0:
+    for p in range(2, s):
+        if a % p == 0:
             return False
     return True
 
-for i in range(1, 20):
-    print(i,is_simple(i))
-    
+
+# Проверяем работу нашей функции
+for n in range(1, 20):
+    print(n, is_simple(n))
+
 
