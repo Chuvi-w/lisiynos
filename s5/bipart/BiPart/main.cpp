ab6d697ab0561119af6c328e25bc2a10898bd8a2
Denis Stepulenok <super.denis@gmail.com>
2014-07-24 11:37:25 +0400

    Save changes

---
+++ s5/bipart/BiPart/main.cpp
@@ -0,0 +1,38 @@
+// Максимальное паросочетание
+#include <iostream>
+#include <assert.h>
+#include <stdio.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+const int maxN = 101;
+
+vector<int> g[maxN];
+
+int m, n, k;
+
+// -1 если нет ребра и номер вершины из
+// A если есть ребро
+int back[maxN];
+
+int main() {
+    freopen("bipart.in","r",stdin);
+    //freopen("bipart.out","w",stdout);
+    // m - вершин в A
+    // n - вершин в B
+    // k - рёбер из A в B
+    cin >> m >> n >> k;
+
+    // Заполняем массив back -1
+    fill(back, back + maxN, -1);
+
+    // Вызываем dfs...
+    // ...дописать...
+
+    return 0;
+}
